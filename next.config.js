6c826fa6b781dacb0580b059c0d4b8c597631aef
Matteo Giampaolo <doyouthinkysauros@gmail.com>
2023-10-02 14:40:42 +0700

    small changes to next config

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode:true,
-    webpack: (config, { isServer }) => {
-        config.resolve.alias['@'] = path.join(__dirname,"src");
-        return config
-    },
     images:{
         domains:["firebasestorage.googleapis.com"]
     }
