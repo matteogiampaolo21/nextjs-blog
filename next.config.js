aa904dd2ceea378e99db7bf4a239994244f2d369
Matteo Giampaolo <doyouthinkysauros@gmail.com>
2023-10-02 14:38:00 +0700

    Removed netlify dependencies and files

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode:true,
+    webpack: (config, { isServer }) => {
+        config.resolve.alias['@'] = path.join(__dirname,"src");
+        return config
+    },
     images:{
         domains:["firebasestorage.googleapis.com"]
     }
