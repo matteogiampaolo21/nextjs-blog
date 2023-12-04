7a2a4f1189793fb1e94298f0909a676deb9fd713
Matteo Giampaolo <doyouthinkysauros@gmail.com>
2023-12-04 12:04:03 +0700

    added small change to button on home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
 import { metadata } from "./layout";
-
+import Link from "next/link";
 export default function Home() {
   metadata.title = "Home"
   return (
     <main className='xl:w-1280 lg:w-1024 md:w-768 sm:w-640 w-320 mx-auto mb-10'>
       <article className='bg-neutral-200 border-2 text-black border-black w-full  mx-auto p-10 rounded shadow-md mt-10'>
         <h1 className='text-4xl mb-5'>Hello there, welcome!</h1>
-        <p className="text-neutral-700">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et labore commodi excepturi quod, reiciendis dicta tempore beatae minima. Laboriosam, repellat!</p>
-        <button className='bg-indigo-500 text-white font-bold border-black border-2 px-3 py-1 text-lg rounded mt-5 hover:bg-indigo-600 duration-300'>View Page</button>
+        <p className="text-neutral-700 mb-5">Lorem ipsum dolor sit amet consectetur adipisicing elit. Et labore commodi excepturi quod, reiciendis dicta tempore beatae minima. Laboriosam, repellat!</p>
+        <Link href={'/register'} className={'bg-indigo-500 text-white font-bold focus:bg-indigo-300 focus:text-neutral-800 border-black border-2 px-3 py-2 text-lg rounded mt-5 hover:bg-indigo-600 duration-300'}>Register Now!</Link>
       </article>
 
       <section className='grid grid-cols-2 gap-10 mx-auto mt-10 xl:w-1280'>
